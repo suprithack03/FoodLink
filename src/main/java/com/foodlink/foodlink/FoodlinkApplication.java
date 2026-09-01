808ed72d0c75975ace7dd0193a426333a3bbd3d6
supritha <suprithack03@gmail.com>
2026-09-01 18:42:13 +0530

    Initial FoodLink project setup

---
+++ src/main/java/com/foodlink/foodlink/FoodlinkApplication.java
@@ -0,0 +1,13 @@
+package com.foodlink.foodlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodlink/foodlink/FoodlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodlink.foodlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
